d6d72e9de46099b6157416beb28f0a7c38ff4bed
Felipe Fonseca <feli13pe@gmail.com>
2024-11-11 16:11:39 -0500

    se agregaron los metodos ordenar hijos, y opcion eliminar person

--- PersonaReal.py
+++ PersonaReal.py
@@ -1,32 +1,33 @@
 class PersonaReal:
-    def __init__(self, nombre, is_king):
+    def __init__(self, nombre, is_king,año_nacimiento, genero):
         self.nombre = nombre
+        self.ano_nacimiento = año_nacimiento
+        self.genero = genero
         self.hijos = []
         self.is_king = is_king
-        self.posicion = 0
-        self.nivel = 0
-
+        self.descendencia = []
 
     def añadir_hijos(self, hijo):
         self.hijos.append(hijo)
 
-    def definir_posicion(self):
-        if self.is_king == True:
-            self.posicion = 0
-
+    def ordenar_hijos(self):
         if len(self.hijos) > 0:
-            if len(self.hijos) > 1:
-                self.hijos[0].posicion = self.posicion +1
-                for i in range(len(self.hijos)-1):
-                    self.hijos[i+1].posicion = self.hijos[i].posicion +1
+
+            for i in range(len(self.hijos)):
+                if self.hijos[i].ano_nacimiento < 2011 and self.hijos[i].genero == "M":
+                    container = self.hijos[i]
+                    self.hijos.remove(self.hijos[i])
+                    self.hijos.append(container)
 
 
-    def nivel(self):
-        if self.is_king == True:
-            self.nivel = 0
-        for i in range(len(self.hijos)):
-            self.hijos[i].nivel = self.nivel + 1
+    def iterar_descendencia(self):
+        # Agregamos una función auxiliar para realizar la recursión
+        self.ordenar_hijos()
+        def agregar_descendencia(persona):
+            if len(persona.hijos) > 0:
+                for hijo in persona.hijos:
+                    self.descendencia.append(hijo)
+                    agregar_descendencia(hijo)
 
-
-
-
+        agregar_descendencia(self)  # Llamamos a la función auxiliar con la persona actual
+        return self.descendencia
--- app.py
+++ app.py
@@ -1,22 +1,118 @@
 from PersonaReal import PersonaReal
 
-isabel = PersonaReal("Reina Isabel II", is_king=True,)
-carlos = PersonaReal("Carlos III", is_king=False)
-andres = PersonaReal("andres", is_king=False)
-eduardo = PersonaReal("Eduardo", is_king=False)
-ana = PersonaReal("Ana", is_king=False)
-henry = PersonaReal("Henry", is_king=False)
-pancho = PersonaReal("Pancho", is_king=False)
 
-isabel.añadir_hijos(carlos)
-isabel.añadir_hijos(andres)
-isabel.añadir_hijos(eduardo)
-isabel.añadir_hijos(ana)
+isabel = PersonaReal("Reina Isabel II", True,1926, "M")
 
-carlos.añadir_hijos(henry)
-isabel.definir_posicion()
+#descendencia isabel
+
+charles = PersonaReal("Charles III ", True, 1948, "H")
+anna = PersonaReal(" Anne, Princess Royal ", False,1950, "M")
+andrew = PersonaReal(" Prince Andrew, Duke of York", False, 1960, "H")
+edward = PersonaReal(" Prince Edward, Duke of Edinburgh ", False, 1964, "H")
+
+#los nacidos antes 2011, los hombres tiene prioridad en el orden de sucesion a la corona britanica antes que las mujeres
+
+isabel.añadir_hijos(charles)
+isabel.añadir_hijos(anna)
+isabel.añadir_hijos(andrew)
+isabel.añadir_hijos(edward)
 
 
-print(isabel.posicion)
-print(carlos.posicion)
-print(andres.posicion)
+#descendencia de charles
+
+william = PersonaReal("William, Prince of Wales", False, 1982, "H")
+harry = PersonaReal("Prince Harry, Duke of Sussex ", False, 1984, "H")
+
+charles.añadir_hijos(william)
+charles.añadir_hijos(harry)
+
+#descendencia de william
+Geroge = PersonaReal("Prince George of Wales", False, 2013, "H")
+charlotte = PersonaReal(" Princess Charlotte of Wales", False, 2015,"M")
+louis = PersonaReal("Prince Louis of Wales ", False, 2018, "H")
+
+william.añadir_hijos(Geroge)
+william.añadir_hijos(charlotte)
+william.añadir_hijos(louis)
+
+#descendencia de Harry
+archie= PersonaReal("Prince Archie of Sussex", False, 2019, "H")
+lilibet = PersonaReal(" Princess Lilibet of Sussex",False, 2021, "M")
+
+harry.añadir_hijos(archie)
+harry.añadir_hijos(lilibet)
+
+
+#descendencia de Andrew
+beatrice = PersonaReal(" Princess Beatrice", False, 1988, "M")
+eugenie = PersonaReal(" Princess Eugenie", False, 1990, "M")
+
+andrew.añadir_hijos(beatrice)
+andrew.añadir_hijos(eugenie)
+
+# descendencia de Beatrice
+sienna = PersonaReal("Sienna Mapelli Mozz", False,2021, "M")
+beatrice.añadir_hijos(sienna)
+
+# descendencia de eugenie
+august = PersonaReal("August Brooksbank",False,2021, "M")
+ernest = PersonaReal("Ernest Brooksbank",False, 2023, "H")
+
+eugenie.añadir_hijos(august)
+eugenie.añadir_hijos(ernest)
+
+#descendencia de Edward
+james = PersonaReal("James Mountbatten-Windsor, Earl of Wessex", False, 2007, "H")
+lady = PersonaReal("Lady Louise Mountbatten-Windsor ", False, 2003, "M")
+
+edward.añadir_hijos(james)
+edward.añadir_hijos(lady)
+
+#descendencia de Anna
+peter = PersonaReal("Peter Phillips", False,1977,"H")
+zara = PersonaReal("Zara Tindall", False, 1981, "M")
+
+anna.añadir_hijos(peter)
+anna.añadir_hijos(zara)
+
+#descendencia Peter:
+savannah = PersonaReal("Savannah Phillips ", False, 2010, "H")
+isla =PersonaReal(" Isla Phillips ", False, 2012, "M")
+
+peter.añadir_hijos(savannah)
+peter.añadir_hijos(isla)
+
+#descendencia Zara
+
+mia = PersonaReal(" Mia Tindall ", False, 2014, "M")
+lena = PersonaReal(" Lena Tindall", False, 2018, "M")
+lucas = PersonaReal ("Lucas Tindall", False, 2021, "H")
+
+zara.añadir_hijos(mia)
+zara .añadir_hijos(lena)
+zara.añadir_hijos(lucas)
+
+## Preguntar al usuario que posicion quiere saber
+userOption = int(input("Ingrese la posición del trono Britanico que desea saber :"))
+
+#Lista externa para guardar la descendencia del antiguo rey, en este caso es isabell
+lista = isabel.iterar_descendencia()
+
+#mostramos al usuario el resultado de su busqueda
+print(lista[userOption].nombre)
+
+## Eliminar a alguien perteneciente a la sucesion del trono
+
+userOption = int(input("Desea eliminar a alguien perteneciente a la cadena de sucesion: \n"
+                   "Escriba 1, si desea eliminar: "))
+
+if userOption == 1:
+    nombre_eliminar = input("Ingrese el nombre de la persona a eliminar: ")
+
+    for hijo in lista:
+        if hijo.nombre == nombre_eliminar:
+            lista.remove(hijo)
+
+    print("Sucesion al Trono Britanico: \n")
+    for i in range(1, len(lista)):
+        print(i, lista[i].nombre)
